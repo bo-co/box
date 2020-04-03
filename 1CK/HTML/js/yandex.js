d4419c8221b83ac4eb8eb8a32f37f2d28e7eba27
bo-co <bo-co@ya.ru>
2020-04-03 23:26:39 +0300

    update

--- 1CK/HTML/js/yandex.js
+++ 1CK/HTML/js/yandex.js
@@ -1,8 +1,9 @@
 ymaps.ready(init);
 
 function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [52.574699, 39.615821],
+  "use strict";
+  var myMap = new ymaps.Map('map', {
+    center: [52.574399, 39.616821],
     zoom: 17,
     controls: [],
     behaviors: []
@@ -12,12 +13,10 @@ function init() {
   });
 
   myMap.geoObjects.add(new ymaps.Placemark([52.574699, 39.615821], {
-    balloonContent: '<strong>Первая сетевая компания</strong><br />г. Лицепк, улица Суворова, 28',
-    iconCaption: 'Первая сетевая компания'
+    iconCaption: 'г. Лицепк, улица Суворова, 28'
   }, {
     preset: 'islands#circleDotIcon',
     iconColor: '#C83834',
-    balloonCloseButton: false,
-    hideIconOnBalloonOpen: false
+    iconOffset: [-8, 8]
   }));
 }
