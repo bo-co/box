b0edb9633b77392cdfc94d43bc50fc0f80564d92
bo-co <bo-co@ya.ru>
2020-04-26 19:10:38 +0300

    summary

--- IP/HTML/js/jquery.product.js
+++ IP/HTML/js/jquery.product.js
@@ -12,30 +12,10 @@ $(document).ready(function () {
       $("div.baron.baron-article > div.baron-scroller").scrollTo($("#" + $(this).data("move")), 600);
     }
   });
-  $(".product-quantity-button").on("click", function () {
-    quantity = parseInt($(".buy-info-div-form-quantity-input." + $(this).data("form")).data("value"));
-    if ($(this).hasClass("plus")) {
-      quantity = quantity + 1;
-      quantity_user = quantity;
-      if (!$(this).parent(".buy-info-div-form-quantity").hasClass("selected")) {
-        $(this).parent(".buy-info-div-form-quantity").addClass("selected");
-      }
-    } else {
-      quantity = quantity - 1;
-      if (quantity < 1) {
-        quantity = 0;
-        quantity_user = 1;
-        if ($(this).parent(".buy-info-div-form-quantity").hasClass("selected")) {
-          $(this).parent(".buy-info-div-form-quantity").removeClass("selected");
-        }
-      } else {
-        quantity_user = quantity;
-      }
+  $(".buy-info-div-form-button").on("click", function () {
+    if ($(this).hasClass("basket") && !$(this).hasClass("selected")) {
+      $(this).addClass("selected");
     }
-    $(".buy-info-div-form-quantity-input." + $(this).data("form")).val(quantity_user);
-    $(".buy-info-div-form-quantity-input." + $(this).data("form")).data({
-      "value": quantity
-    });
   });
 
 
